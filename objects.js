acb61de180cdaabd2bea617803d339aea800c5a9
aditi <aditi.tantradnyan@gmail.com>
2021-04-29 22:38:09 +0530

    Objects

---
+++ objects.js
@@ -0,0 +1,19 @@
+let ganpatiMakharTypes={
+type:'Velvet',
+color:['Dark Purple','Red','Orange'],
+cost: 1500
+};
+/*
+objectname.propertyname
+objectname['propertyname']
+*/
+console.log(ganpatiMakharTypes.cost);
+console.log(ganpatiMakharTypes['cost']);
+console.log(ganpatiMakharTypes.color[2]);
+/*
+Output
+
+1500
+1500
+*/
+
