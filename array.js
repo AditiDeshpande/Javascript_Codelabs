36c88c3f5a779be4843105fedf24f92b87f8f75f
aditi <aditi.tantradnyan@gmail.com>
2021-04-29 16:56:10 +0530

    Mix array

--- array.js
+++ array.js
@@ -6,3 +6,20 @@ let numbers=[1,21,13,47,566,5,6];
 console.log(numbers);
 console.log(numbers[2]+numbers[4]);
 console.log(numbers.sort((a,b)=>a-b));
+numbers.push(-33);
+console.log(numbers);
+console.log(numbers.sort((a,b)=>a-b));
+
+/*
+Output
+
+["Paintbrush", "Pencil", "Colours", "ColourPalette", "Canvas"]
+array.js:6 (7) [1, 21, 13, 47, 566, 5, 6]
+array.js:7 579
+array.js:8 (7) [1, 5, 6, 13, 21, 47, 566]
+array.js:10 (8) [1, 5, 6, 13, 21, 47, 566, -33]
+array.js:11 (8) [-33, 1, 5, 6, 13, 21, 47, 566]
+*/
+
+let mix = ["Aditi" , true , 5.5]
+console.log(mix);
