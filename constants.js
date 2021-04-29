b6aa59b4f1c954b2b4f99cf280b847b62beb09ad
aditi <aditi.tantradnyan@gmail.com>
2021-04-29 15:06:00 +0530

    Primitives constants variables

---
+++ constants.js
@@ -0,0 +1,9 @@
+const pi = 3.14
+pi = 3.12
+
+console.log(pi);
+
+/*
+Output
+Uncaught TypeError: Assignment to constant variable.
+*/
---
+++ primitives.js
@@ -0,0 +1,8 @@
+let name = "Harry Potter";
+let age ='1';
+
+console.log(name);
+console.log(age);
+
+/*Whatever in quotes will be treated as a String. So 1 is String.
+*/
