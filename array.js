36a639d4ca71c602de48dad50277a20c067b2c43
aditi <aditi.tantradnyan@gmail.com>
2021-04-29 16:19:23 +0530

    Array sorting

---
+++ array.js
@@ -0,0 +1,8 @@
+let shopping=[];
+shopping=['Paintbrush','Pencil','Colours','ColourPalette','Canvas'];
+console.log(shopping);
+
+let numbers=[1,21,13,47,566,5,6];
+console.log(numbers);
+console.log(numbers[2]+numbers[4]);
+console.log(numbers.sort((a,b)=>a-b));
--- primitives.js
+++ primitives.js
@@ -6,3 +6,14 @@ console.log(age);
 
 /*Whatever in quotes will be treated as a String. So 1 is String.
 */
+
+let option = true;
+let height;
+console.log(option);
+console.log(height);
+
+/*
+output for log of height is undefined
+*/
+
+let eyecolour = null;
